8c306994164ea7988ce90650ab4798485cbb6b5a
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-11 21:47:28 -0300

    asynchronous error handling added

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
-const routes = require("./routes")
+require("express-async-erros");
+const AppError = require("./utils/AppError")
+const routes = require("./routes");
 const express = require("express");
 const app = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use(routes);
 
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+      statusCode: error.statusCode
+    })
+  };
 
+  console.error(error)
 
-
-
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+    statusCode: 500
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => {console.log(`Server running at port: ${PORT}`)});
