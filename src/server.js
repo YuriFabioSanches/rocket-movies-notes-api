10b4cc8109e55700a39dd2f85729307ef1121d6a
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-11 21:39:09 -0300

    index routes created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+const routes = require("./routes")
 const express = require("express");
 const app = express();
 
-
+app.use(express.json())
+app.use(routes)
 
 
 
@@ -9,4 +11,4 @@ const app = express();
 
 
 const PORT = 3333;
-app.listen(PORT, () => {console.log(`Server running at port: ${PORT}`)})
+app.listen(PORT, () => {console.log(`Server running at port: ${PORT}`)});
