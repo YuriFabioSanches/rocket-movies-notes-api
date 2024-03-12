fb3b4f69bfafd3495deccb15650c552b8a8ae93f
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-12 18:48:48 -0300

    created get all tags for one user function

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const routes = Router();
 
 const usersRoutes = require("./usersRoutes");
 const movieNotesRoutes = require("./movieNotesRoutes");
+const movieTagsRoutes = require("./movieTagsRoutes");
 
 routes.use("/users", usersRoutes);
 routes.use("/movienotes", movieNotesRoutes);
+routes.use("/movietags", movieTagsRoutes);
 
 module.exports = routes;
