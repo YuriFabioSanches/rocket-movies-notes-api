38dbad11c9c696a6f38a8c3703d1a9428f9a1d49
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-12 16:14:09 -0300

    created get by id movie note function and rating middleware

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -1,6 +1,23 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 class MovieNotesController {
+  async show(request, response) {
+    const { id } = request.params;
+
+    const movie_note = await knex("movie_notes").where({ id }).first();
+    
+    if(!movie_note){
+      throw new AppError("Note don't exist.");
+    };
+
+    const movie_note_tags = await knex("movie_tags").where({ note_id: id });
+
+    return response.status(200).json({
+      ...movie_note,
+      movie_note_tags
+    });
+  };
+
   async create(request, response) {
     const { title, description, rating, tags } = request.body;
     const { user_id } = request.params;
@@ -8,10 +25,6 @@ class MovieNotesController {
     const [checkUserExist] = await knex("users").where("id", user_id);
     if(!checkUserExist){
       throw new AppError("User not found.")
-    };
-
-    if(rating < 1 || rating > 5){
-      throw new AppError("Rating need to be bettwen 1 and 5.")
     };
 
     const [note_id] = await knex("movie_notes").insert({
@@ -37,6 +50,19 @@ class MovieNotesController {
 
     return response.status(201).json({message: "Movie note created"});
   };
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    const [noteExist] = await knex("movie_notes").where({ id })
+    if(!noteExist){
+      throw new AppError("Movie note does not exist.")
+    }
+
+    await knex("movie_notes").where({ id }).delete()
+
+    return response.status(200).json({message: "Movie note deleted"})
+  }
 };
 
 module.exports = MovieNotesController;
--- src/routes/movieNotesRoutes.js
+++ src/routes/movieNotesRoutes.js
@@ -1,8 +1,11 @@
+const ratingMiddleware = require("../utils/ratingMiddleware")
 const { Router } = require("express");
 const movieNotesRoutes = Router();
 const MovieNotesController = require("../controllers/MovieNotesController");
 const movieNotesController = new MovieNotesController();
 
-movieNotesRoutes.post("/:user_id", movieNotesController.create);
+movieNotesRoutes.get("/:id", movieNotesController.show)
+movieNotesRoutes.post("/:user_id", ratingMiddleware, movieNotesController.create);
+movieNotesRoutes.delete("/:id", movieNotesController.delete)
 
 module.exports = movieNotesRoutes;
