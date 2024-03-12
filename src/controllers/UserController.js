f7dfc585b38553929587806c8316e6a4b13dbcba
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-12 11:40:30 -0300

    created user update function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UserController {
   async create(request, response) {
@@ -24,7 +24,52 @@ class UserController {
     await knex("users").insert({ name, email, password:hashPassword });
 
     return response.status(201).json({message: "User created"});
-  }
+  };
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const [user] = await knex("users").where({ id });
+
+    if(!user){
+      throw new AppError("User not found.");
+    };
+
+    if(email) {
+      const [userWithEmailInUse] = await knex("users").where({ email });
+  
+      if(userWithEmailInUse && userWithEmailInUse.id != user.id) {
+        throw new AppError("E-mail already registered.");
+      };
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(!password && old_password){
+      throw new AppError("Please inform a new password.");
+    }else if(password && !old_password){
+      throw new AppError("Please inform your old password.");
+    };
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword){
+        throw new AppError("Your old password is wrong.");
+      }
+
+      const newHashPassword = await hash(password, 8);
+      user.password = newHashPassword;
+    };
+
+    user.updated_at = knex.fn.now();
+
+    await knex("users").update(user).where({ id });
+
+    return response.status(200).json({message: "User updated"});
+  };
 };
 
 module.exports = UserController;
